6d887ebe08c39ec6c93b3c7d0f746ea6663bc6c2
Paula Regiori Feres Alves <paula.regioriferesalves@MacBook-Pro-van-Paula.local>
2023-09-03 08:57:01 +0200

    New custom theme

---
+++ web/themes/portfolio/js/portfolio.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * portfolio behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.portfolio = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
