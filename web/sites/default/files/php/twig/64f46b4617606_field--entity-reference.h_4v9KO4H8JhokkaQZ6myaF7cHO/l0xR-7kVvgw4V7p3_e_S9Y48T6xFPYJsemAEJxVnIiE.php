022ed62a9582fbfcabc1a5111de838435307ccd4
Paula Regiori Feres Alves <paula.regioriferesalves@MacBook-Pro-van-Paula.local>
2023-09-03 13:20:55 +0200

    Theme color and layout changes

---
+++ web/sites/default/files/php/twig/64f46b4617606_field--entity-reference.h_4v9KO4H8JhokkaQZ6myaF7cHO/l0xR-7kVvgw4V7p3_e_S9Y48T6xFPYJsemAEJxVnIiE.php
@@ -0,0 +1,254 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/contrib/showcase_lite/templates/field--entity-reference.html.twig */
+class __TwigTemplate_8a5b9345a626827568d216e31e82e7d8d1a41f41fd1db8e881396f532a1263cc extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 25
+        $context["classes"] = [0 => "field", 1 => ("field--name-" . \Drupal\Component\Utility\Html::getClass($this->sandbox->ensureToStringAllowed(        // line 27
+($context["field_name"] ?? null), 27, $this->source))), 2 => ("field--type-" . \Drupal\Component\Utility\Html::getClass($this->sandbox->ensureToStringAllowed(        // line 28
+($context["field_type"] ?? null), 28, $this->source))), 3 => ("field--label-" . $this->sandbox->ensureToStringAllowed(        // line 29
+($context["label_display"] ?? null), 29, $this->source)), 4 => ("field--entity-reference-target-type-" . \Drupal\Component\Utility\Html::getClass($this->sandbox->ensureToStringAllowed(        // line 30
+($context["entity_reference_target_type"] ?? null), 30, $this->source))), 5 => "clearfix"];
+        // line 35
+        $context["title_classes"] = [0 => "field__label", 1 => (((        // line 37
+($context["label_display"] ?? null) == "inline")) ? ("inline") : (""))];
+        // line 40
+        if (($context["label_hidden"] ?? null)) {
+            // line 41
+            echo "  ";
+            if (($context["multiple"] ?? null)) {
+                // line 42
+                echo "    <div";
+                echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["attributes"] ?? null), "addClass", [0 => ($context["classes"] ?? null), 1 => "field__items"], "method", false, false, true, 42), 42, $this->source), "html", null, true);
+                echo ">
+      ";
+                // line 43
+                $context['_parent'] = $context;
+                $context['_seq'] = twig_ensure_traversable(($context["items"] ?? null));
+                foreach ($context['_seq'] as $context["_key"] => $context["item"]) {
+                    // line 44
+                    echo "        <div";
+                    echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, $context["item"], "attributes", [], "any", false, false, true, 44), "addClass", [0 => "field__item"], "method", false, false, true, 44), 44, $this->source), "html", null, true);
+                    echo ">";
+                    echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, $context["item"], "content", [], "any", false, false, true, 44), 44, $this->source), "html", null, true);
+                    echo "</div>
+      ";
+                }
+                $_parent = $context['_parent'];
+                unset($context['_seq'], $context['_iterated'], $context['_key'], $context['item'], $context['_parent'], $context['loop']);
+                $context = array_intersect_key($context, $_parent) + $_parent;
+                // line 46
+                echo "    </div>
+  ";
+            } else {
+                // line 48
+                echo "    ";
+                $context['_parent'] = $context;
+                $context['_seq'] = twig_ensure_traversable(($context["items"] ?? null));
+                foreach ($context['_seq'] as $context["_key"] => $context["item"]) {
+                    // line 49
+                    echo "      <div";
+                    echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["attributes"] ?? null), "addClass", [0 => ($context["classes"] ?? null), 1 => "field__item"], "method", false, false, true, 49), 49, $this->source), "html", null, true);
+                    echo ">";
+                    echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, $context["item"], "content", [], "any", false, false, true, 49), 49, $this->source), "html", null, true);
+                    echo "</div>
+    ";
+                }
+                $_parent = $context['_parent'];
+                unset($context['_seq'], $context['_iterated'], $context['_key'], $context['item'], $context['_parent'], $context['loop']);
+                $context = array_intersect_key($context, $_parent) + $_parent;
+                // line 51
+                echo "  ";
+            }
+        } else {
+            // line 53
+            echo "  <div";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["attributes"] ?? null), "addClass", [0 => ($context["classes"] ?? null)], "method", false, false, true, 53), 53, $this->source), "html", null, true);
+            echo ">
+    <div";
+            // line 54
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["title_attributes"] ?? null), "addClass", [0 => ($context["title_classes"] ?? null)], "method", false, false, true, 54), 54, $this->source), "html", null, true);
+            echo ">";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["label"] ?? null), 54, $this->source), "html", null, true);
+            echo "</div>
+    ";
+            // line 55
+            if (($context["multiple"] ?? null)) {
+                // line 56
+                echo "      <div class=\"field__items\">
+    ";
+            }
+            // line 58
+            echo "    ";
+            $context['_parent'] = $context;
+            $context['_seq'] = twig_ensure_traversable(($context["items"] ?? null));
+            foreach ($context['_seq'] as $context["_key"] => $context["item"]) {
+                // line 59
+                echo "      <div";
+                echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, $context["item"], "attributes", [], "any", false, false, true, 59), "addClass", [0 => "field__item"], "method", false, false, true, 59), 59, $this->source), "html", null, true);
+                echo ">";
+                echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, $context["item"], "content", [], "any", false, false, true, 59), 59, $this->source), "html", null, true);
+                echo "</div>
+    ";
+            }
+            $_parent = $context['_parent'];
+            unset($context['_seq'], $context['_iterated'], $context['_key'], $context['item'], $context['_parent'], $context['loop']);
+            $context = array_intersect_key($context, $_parent) + $_parent;
+            // line 61
+            echo "    ";
+            if (($context["multiple"] ?? null)) {
+                // line 62
+                echo "      </div>
+    ";
+            }
+            // line 64
+            echo "  </div>
+";
+        }
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/contrib/showcase_lite/templates/field--entity-reference.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  137 => 64,  133 => 62,  130 => 61,  119 => 59,  114 => 58,  110 => 56,  108 => 55,  102 => 54,  97 => 53,  93 => 51,  82 => 49,  77 => 48,  73 => 46,  62 => 44,  58 => 43,  53 => 42,  50 => 41,  48 => 40,  46 => 37,  45 => 35,  43 => 30,  42 => 29,  41 => 28,  40 => 27,  39 => 25,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{#
+/**
+ * @file
+ * Showcase Lite's theme override for taxonomy term fields.
+ *
+ * Available variables:
+ * - attributes: HTML attributes for the containing element.
+ * - label_hidden: Whether to show the field label or not.
+ * - title_attributes: HTML attributes for the label.
+ * - label: The label for the field.
+ * - content_attributes: HTML attributes for the content.
+ * - items: List of all the field items. Each item contains:
+ *   - attributes: List of HTML attributes for each item.
+ *   - content: The field item's content.
+ * - entity_type: The entity type to which the field belongs.
+ * - field_name: The name of the field.
+ * - field_type: The type of the field.
+ * - label_display: The display settings for the label.
+ *
+ * @see template_preprocess_field()
+ * @see bartik_preprocess_field()
+ */
+#}
+{%
+  set classes = [
+    'field',
+    'field--name-' ~ field_name|clean_class,
+    'field--type-' ~ field_type|clean_class,
+    'field--label-' ~ label_display,
+    'field--entity-reference-target-type-' ~ entity_reference_target_type|clean_class,
+    'clearfix',
+  ]
+%}
+{%
+  set title_classes = [
+    'field__label',
+    label_display == 'inline' ? 'inline',
+  ]
+%}
+{% if label_hidden %}
+  {% if multiple %}
+    <div{{ attributes.addClass(classes, 'field__items') }}>
+      {% for item in items %}
+        <div{{ item.attributes.addClass('field__item') }}>{{ item.content }}</div>
+      {% endfor %}
+    </div>
+  {% else %}
+    {% for item in items %}
+      <div{{ attributes.addClass(classes, 'field__item') }}>{{ item.content }}</div>
+    {% endfor %}
+  {% endif %}
+{% else %}
+  <div{{ attributes.addClass(classes) }}>
+    <div{{ title_attributes.addClass(title_classes) }}>{{ label }}</div>
+    {% if multiple %}
+      <div class=\"field__items\">
+    {% endif %}
+    {% for item in items %}
+      <div{{ item.attributes.addClass('field__item') }}>{{ item.content }}</div>
+    {% endfor %}
+    {% if multiple %}
+      </div>
+    {% endif %}
+  </div>
+{% endif %}
+", "themes/contrib/showcase_lite/templates/field--entity-reference.html.twig", "/app/web/themes/contrib/showcase_lite/templates/field--entity-reference.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("set" => 25, "if" => 40, "for" => 43);
+        static $filters = array("clean_class" => 27, "escape" => 42);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['set', 'if', 'for'],
+                ['clean_class', 'escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
